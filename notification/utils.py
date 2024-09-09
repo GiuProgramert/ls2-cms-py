8f0620023f4cf9bf27717f6ae161aa90d4946384
Andres Roman <androceomimo@fpuna.edu.py>
2024-09-09 13:41:13 -0400

    actualizacion de la funcion send_email

--- notification/utils.py
+++ notification/utils.py
@@ -8,14 +8,13 @@ def send_email(to, subject, html):
     try:
         params: resend.Emails.SendParams = {
             "from": "Acme <onboarding@resend.dev>",
-            "to": ["giulidiazp@fpuna.edu.py"],
-            "subject": "hello world",
-            "html": "<strong>it works!</strong>",
+            "to": [to],
+            "subject": subject,
+            "html": html,
         }
-
         email = resend.Emails.send(params)
-
         return email
     except Exception as Error:
         print("No se pudo enviar el mail")
         print(Error)
+
