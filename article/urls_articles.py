357579465cbd2c91beba2734f723e51b97369602
Andres Roman <androceomimo@fpuna.edu.py>
2024-10-13 14:05:39 -0300

    vista del rol financiero que va a ser un grafico con las categorias mas vendidas de mayor a menor

--- article/urls_articles.py
+++ article/urls_articles.py
@@ -41,4 +41,5 @@ urlpatterns = [
     ),
     path("<int:pk>/like/", views.like_article, name="like-article"),
     path("<int:pk>/dislike/", views.dislike_article, name="dislike-article"),
+    path("sold-categories/", views.sold_categories, name="sold-categories"),
 ]
